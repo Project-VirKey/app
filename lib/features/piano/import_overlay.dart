54ac37352f9f5e380d917f1231a49c8269f5965a
Richard Krikler <57064056+RichardKrikler@users.noreply.github.com>
2022-11-14 11:20:04 +0100

    added SafeArea back for Import-Overlay in Landscape-Mode

--- lib/features/piano/import_overlay.dart
+++ lib/features/piano/import_overlay.dart
@@ -9,58 +9,55 @@ import 'package:virkey/constants/fonts.dart';
 import 'package:virkey/constants/radius.dart';
 
 OverlayEntry importOverlay = OverlayEntry(builder: (context) {
-  return OrientationBuilder(builder: (context, orientation) {
-    return AppKeyboardShortcut(
-      shortcuts: {
-        PhysicalKeyboardKey.escape: () => importOverlay.remove(),
-      },
-      child: Container(
-        color: AppColors.black50,
-        child: SafeArea(
-          bottom: orientation == Orientation.portrait,
-          child: Container(
-            margin: const EdgeInsets.all(11),
-            decoration: const BoxDecoration(
-              color: AppColors.white,
-              borderRadius: BorderRadius.all(AppRadius.radius),
-            ),
-            child: Padding(
-              padding: const EdgeInsets.all(11),
-              child: Column(
-                children: [
-                  SizedBox(
-                    width: MediaQuery.of(context).size.width,
-                    height: 40,
-                    child: Stack(
-                      children: [
-                        const Positioned.fill(
-                            left: 0,
-                            right: 0,
-                            child: Center(
-                              child: AppText(
-                                  text: 'Import',
-                                  size: 30,
-                                  family: AppFonts.secondary),
-                            )),
-                        Positioned(
+  return AppKeyboardShortcut(
+    shortcuts: {
+      PhysicalKeyboardKey.escape: () => importOverlay.remove(),
+    },
+    child: Container(
+      color: AppColors.black50,
+      child: SafeArea(
+        child: Container(
+          margin: const EdgeInsets.all(11),
+          decoration: const BoxDecoration(
+            color: AppColors.white,
+            borderRadius: BorderRadius.all(AppRadius.radius),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(11),
+            child: Column(
+              children: [
+                SizedBox(
+                  width: MediaQuery.of(context).size.width,
+                  height: 40,
+                  child: Stack(
+                    children: [
+                      const Positioned.fill(
                           left: 0,
-                          child: Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: AppIcon(
-                                icon: HeroIcons.arrowUturnLeft,
-                                color: AppColors.dark,
-                                onPressed: () => importOverlay.remove()),
-                          ),
+                          right: 0,
+                          child: Center(
+                            child: AppText(
+                                text: 'Import',
+                                size: 30,
+                                family: AppFonts.secondary),
+                          )),
+                      Positioned(
+                        left: 0,
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: AppIcon(
+                              icon: HeroIcons.arrowUturnLeft,
+                              color: AppColors.dark,
+                              onPressed: () => importOverlay.remove()),
                         ),
-                      ],
-                    ),
-                  )
-                ],
-              ),
+                      ),
+                    ],
+                  ),
+                )
+              ],
             ),
           ),
         ),
       ),
-    );
-  });
+    ),
+  );
 });
