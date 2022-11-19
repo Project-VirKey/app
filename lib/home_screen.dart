2ec55e741800453815c81d66de58d113c484f3e2
Richard Krikler <57064056+RichardKrikler@users.noreply.github.com>
2022-11-19 21:11:17 +0100

    ListView: expanded properties

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:go_router/go_router.dart';
 import 'package:heroicons/heroicons.dart';
+import 'package:virkey/common_widgets/app_button.dart';
 import 'package:virkey/common_widgets/app_icon.dart';
 import 'package:virkey/common_widgets/app_shadow.dart';
 import 'package:virkey/common_widgets/app_text.dart';
@@ -23,6 +24,73 @@ class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
     // https://medium.flutterdevs.com/implemented-overlay-in-flutter-fe60d2b33a04
     SettingsOverlay settingsOverlay =
         SettingsOverlay(context: context, vsync: this);
+
+    const itemsRep = [
+      'Rec1',
+      'Rec2',
+      'Rec3',
+      'Rec4',
+      'Rec5',
+      'Rec6',
+      'Rec7',
+      'Rec8',
+      'Rec9'
+    ];
+    final items = List.of(itemsRep);
+    final recordingsListKey = GlobalKey<AnimatedListState>();
+    bool expandedItem = false;
+
+    void addItem(String value) {
+      items.insert(0, value);
+      recordingsListKey.currentState!
+          .insertItem(0, duration: const Duration(milliseconds: 150));
+    }
+
+    void removeItem(int index) {
+      recordingsListKey.currentState!.removeItem(0, (context, animation) {
+        return SizeTransition(
+          sizeFactor: animation,
+          child: Card(
+              color: AppColors.secondary,
+              child: Container(
+                height: 20,
+              )),
+        );
+      }, duration: const Duration(milliseconds: 150));
+      items.removeAt(index);
+    }
+
+    void removeAllItems() {
+      for (var i = 0; i <= items.length - 1; i++) {
+        recordingsListKey.currentState?.removeItem(0,
+            (BuildContext context, Animation<double> animation) {
+          return SizeTransition(
+            sizeFactor: animation,
+            child: Card(
+                color: AppColors.secondary,
+                child: Container(
+                  height: 20,
+                )),
+          );
+        }, duration: const Duration(milliseconds: 150));
+      }
+      items.clear();
+    }
+
+    void expandItem(int index) {
+      String item = items[index];
+      removeAllItems();
+      addItem(item);
+      expandedItem = true;
+    }
+
+    void contractItem() {
+      removeAllItems();
+      for (var element in itemsRep.reversed) {
+        addItem(element);
+      }
+      expandedItem = false;
+    }
 
     return Scaffold(
       backgroundColor: AppColors.secondary,
@@ -142,6 +210,7 @@ class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
                       text: 'Recordings',
                       color: AppColors.secondary,
                       size: 26,
+                      letterSpacing: 3,
                       weight: AppFonts.weightLight,
                       textAlign: TextAlign.center,
                     ),
@@ -151,7 +220,7 @@ class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
             ],
           ),
           Expanded(
-            // Expanded -> contain LisView (https://daill.de/flutter-handle-listview-overflow-in-column)
+            // Expanded -> contain ListView (https://daill.de/flutter-handle-listview-overflow-in-column)
             child: NotificationListener<UserScrollNotification>(
               onNotification: (notification) {
                 // if (notification.direction is ScrollDirection.forward) {
@@ -159,60 +228,178 @@ class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
                 // }
                 return true;
               },
-              child: ListView.separated(
+              child: AnimatedList(
+                key: recordingsListKey,
                 scrollDirection: Axis.vertical,
                 shrinkWrap: true,
-                itemBuilder: (BuildContext context, int index) {
-                  return Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Expanded(
-                        child: Container(
-                          margin: const EdgeInsets.symmetric(horizontal: 30),
-                          child: TextButton(
-                            onPressed: () => {print('expand #$index')},
-                            style: TextButton.styleFrom(
-                              backgroundColor: Colors.transparent,
-                              foregroundColor: AppColors.dark,
-                              padding: const EdgeInsets.symmetric(
-                                  horizontal: 8, vertical: 14),
-                            ),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                AppShadow(
-                                  child: AppText(
-                                    text: 'Recording #${index + 1}',
-                                    size: 18,
+                initialItemCount: items.length,
+                itemBuilder: (context, index, animation) {
+                  return SizeTransition(
+                    key: UniqueKey(),
+                    sizeFactor: animation,
+                    // position: animation.drive(
+                    //   Tween(
+                    //     begin: const Offset(0, 1),
+                    //     end: Offset.zero,
+                    //   ),
+                    // ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Expanded(
+                          child: Column(
+                            children: [
+                              Container(
+                                margin:
+                                    const EdgeInsets.symmetric(horizontal: 30),
+                                child: TextButton(
+                                  onPressed: () => {
+                                    if (expandedItem)
+                                      {contractItem()}
+                                    else
+                                      {expandItem(index)}
+                                  },
+                                  style: TextButton.styleFrom(
+                                    backgroundColor: Colors.transparent,
+                                    foregroundColor: AppColors.dark,
+                                    padding: const EdgeInsets.symmetric(
+                                        horizontal: 8, vertical: 14),
+                                  ),
+                                  child: Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      AppShadow(
+                                        child: AppText(
+                                          text: items[index],
+                                          size: 18,
+                                          letterSpacing: 3,
+                                        ),
+                                      ),
+                                      AppIcon(
+                                        icon: expandedItem
+                                            ? HeroIcons.chevronUp
+                                            : HeroIcons.chevronDown,
+                                        color: AppColors.dark,
+                                      ),
+                                    ],
                                   ),
                                 ),
-                                const AppIcon(
-                                  icon: HeroIcons.chevronDown,
-                                  color: AppColors.dark,
-                                ),
-                              ],
-                            ),
+                              ),
+                              Container(
+                                height: 5,
+                                margin:
+                                    const EdgeInsets.symmetric(horizontal: 30),
+                                decoration: const BoxDecoration(
+                                    color: AppColors.tertiary,
+                                    borderRadius:
+                                        BorderRadius.all(Radius.circular(50))),
+                              ),
+                              if (expandedItem)
+                                Container(
+                                  margin: const EdgeInsets.symmetric(
+                                    horizontal: 50,
+                                    vertical: 15,
+                                  ),
+                                  child: Column(
+                                    children: [
+                                      PropertyDescriptionActionCombination(
+                                        title: items[index],
+                                        icon: HeroIcons.pencilSquare,
+                                        onPressed: () => {},
+                                      ),
+                                      const AppText(
+                                        text: 'Audio-Playback',
+                                        size: 16,
+                                        textAlign: TextAlign.center,
+                                      ),
+                                      PropertyDescriptionActionCombination(
+                                        title: 'Audio',
+                                        icon: HeroIcons.arrowDownTray,
+                                        onPressed: () => {},
+                                      ),
+                                      PropertyDescriptionActionCombination(
+                                        title: 'File1.mp3',
+                                        icon: HeroIcons.trash,
+                                        onPressed: () => {},
+                                      ),
+                                      const AppText(
+                                        text: 'Export',
+                                        size: 16,
+                                        textAlign: TextAlign.center,
+                                      ),
+                                      PropertyDescriptionActionCombination(
+                                        title: 'Audio',
+                                        icon: HeroIcons.arrowUpTray,
+                                        onPressed: () => {},
+                                      ),
+                                      PropertyDescriptionActionCombination(
+                                        title: 'MIDI',
+                                        icon: HeroIcons.arrowUpTray,
+                                        onPressed: () => {},
+                                      ),
+                                      PropertyDescriptionActionCombination(
+                                        title: 'Audio & MIDI',
+                                        icon: HeroIcons.arrowUpTray,
+                                        onPressed: () => {},
+                                      ),
+                                      const AppText(
+                                        text: 'Delete',
+                                        size: 16,
+                                        textAlign: TextAlign.center,
+                                      ),
+                                      PropertyDescriptionActionCombination(
+                                        title: 'Delete Recording',
+                                        icon: HeroIcons.trash,
+                                        onPressed: () => {},
+                                      ),
+                                    ],
+                                  ),
+                                )
+                            ],
                           ),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   );
                 },
-                separatorBuilder: (BuildContext context, int index) {
-                  return Container(
-                    height: 5,
-                    margin: const EdgeInsets.symmetric(horizontal: 30),
-                    decoration: const BoxDecoration(
-                        color: AppColors.tertiary,
-                        borderRadius: BorderRadius.all(Radius.circular(50))),
-                  );
-                },
-                itemCount: 30,
               ),
             ),
           ),
         ],
       ),
+    );
+  }
+}
+
+class PropertyDescriptionActionCombination extends StatelessWidget {
+  const PropertyDescriptionActionCombination({
+    Key? key,
+    required this.title,
+    required this.icon,
+    required this.onPressed,
+  }) : super(key: key);
+
+  final String title;
+  final Object icon;
+  final dynamic onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        AppText(
+          text: title,
+          size: 16,
+          letterSpacing: 3,
+          weight: AppFonts.weightLight,
+        ),
+        AppIcon(
+          icon: icon,
+          color: AppColors.dark,
+        )
+      ],
     );
   }
 }
