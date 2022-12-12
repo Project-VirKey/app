56dea1bfea45f3f7d0b0e2c3bf652f369d936515
Richard Krikler <57064056+RichardKrikler@users.noreply.github.com>
2022-12-12 13:24:36 +0100

    cloud: login/signup/password-reset/delete-account/update-username/update-password/update-email/verification

--- lib/features/cloud_synchronisation/authentication.dart
+++ lib/features/cloud_synchronisation/authentication.dart
@@ -1,26 +1,17 @@
 import 'package:firebase_auth/firebase_auth.dart';
 
 class AppAuthentication {
-  static bool loggedIn = false;
-
-  // static void checkAuthStatus() {
-  //   FirebaseAuth.instance.authStateChanges().listen((User? user) {
-  //     if (user == null) {
-  //       print('User is currently signed out!');
-  //       loggedIn = false;
-  //     } else {
-  //       print(user);
-  //       print('User is signed in!');
-  //       loggedIn = true;
-  //     }
-  //   });
-  // }
+  static User? user() {
+    return FirebaseAuth.instance.currentUser;
+  }
 
   static Future<List> logIn(String emailAddress, String password) async {
     try {
       final credential = await FirebaseAuth.instance
           .signInWithEmailAndPassword(email: emailAddress, password: password);
-      return [true, '${credential.user?.displayName ?? 'User'} is logged in.'
+      return [
+        true,
+        '${FirebaseAuth.instance.currentUser?.displayName ?? 'User'} is logged in.'
       ];
     } on FirebaseAuthException catch (e) {
       if (e.code == 'user-not-found') {
@@ -36,5 +27,126 @@ class AppAuthentication {
 
   static void logout() {
     FirebaseAuth.instance.signOut();
+  }
+
+  static Future<List> signUp(
+      String username, String emailAddress, String password) async {
+    try {
+      final credential =
+          await FirebaseAuth.instance.createUserWithEmailAndPassword(
+        email: emailAddress,
+        password: password,
+      );
+
+      await FirebaseAuth.instance.currentUser?.updateDisplayName(username);
+      return await sendVerificationEmail();
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'weak-password') {
+        print('The password provided is too weak.');
+        return [false, 'The password provided is too weak.'];
+      } else if (e.code == 'email-already-in-use') {
+        print('The account already exists for that email.');
+        return [false, 'The account already exists for that email.'];
+      }
+      return [false, e.code];
+    } catch (e) {
+      print(e);
+      return [false, e];
+    }
+  }
+
+  static Future<List> sendVerificationEmail() async {
+    try {
+      await FirebaseAuth.instance.currentUser?.sendEmailVerification();
+      return [true, 'Verify E-Mail sent!'];
+    } on FirebaseAuthException catch (e) {
+      print(e.code);
+      return [false, e.code];
+    } catch (e) {
+      print(e);
+      return [false, e];
+    }
+  }
+
+  static Future<List> sendResetPasswordEmail(String emailAddress) async {
+    try {
+      if (emailAddress.isEmpty) {
+        return [false, 'Enter your E-Mail!'];
+      }
+
+      await FirebaseAuth.instance.sendPasswordResetEmail(email: emailAddress);
+      return [true, 'Rest E-mail sent!'];
+    } on FirebaseAuthException catch (e) {
+      print(e.code);
+      return [false, e.code];
+    } catch (e) {
+      print(e);
+      return [false, e];
+    }
+  }
+
+  static Future<List> deleteAccount(String password) async {
+    try {
+      await reauthenticate(password);
+      await FirebaseAuth.instance.currentUser?.delete();
+      return [true, 'Account deleted!'];
+    } on FirebaseAuthException catch (e) {
+      print(e.code);
+      return [false, e.code];
+    } catch (e) {
+      print(e);
+      return [false, e];
+    }
+  }
+
+  static Future<void> reauthenticate(String password) async {
+    await FirebaseAuth.instance.currentUser?.reauthenticateWithCredential(
+        EmailAuthProvider.credential(
+            email: FirebaseAuth.instance.currentUser?.email ?? '',
+            password: password));
+  }
+
+  static Future<List> updatePassword(
+      String password, String newPassword) async {
+    try {
+      await reauthenticate(password);
+      await FirebaseAuth.instance.currentUser?.updatePassword(newPassword);
+      logout();
+      return [true, 'Password updated!'];
+    } on FirebaseAuthException catch (e) {
+      print(e.code);
+      return [false, e.code];
+    } catch (e) {
+      print(e);
+      return [false, e];
+    }
+  }
+
+  static Future<List> updateEmail(String newEmail, String password) async {
+    try {
+      await reauthenticate(password);
+      await FirebaseAuth.instance.currentUser?.updateEmail(newEmail);
+      logout();
+      return [true, 'E-Mail updated!'];
+    } on FirebaseAuthException catch (e) {
+      print(e.code);
+      return [false, e.code];
+    } catch (e) {
+      print(e);
+      return [false, e];
+    }
+  }
+
+  static Future<List> updateUsername(String newUsername) async {
+    try {
+      await FirebaseAuth.instance.currentUser?.updateDisplayName(newUsername);
+      return [true, 'Username updated!'];
+    } on FirebaseAuthException catch (e) {
+      print(e.code);
+      return [false, e.code];
+    } catch (e) {
+      print(e);
+      return [false, e];
+    }
   }
 }
---
+++ lib/features/cloud_synchronisation/update_username_overlay.dart
@@ -0,0 +1,172 @@
+import 'package:email_validator/email_validator.dart';
+import 'package:flutter/material.dart';
+import 'package:heroicons/heroicons.dart';
+import 'package:provider/provider.dart';
+import 'package:virkey/common_widgets/app_button.dart';
+import 'package:virkey/common_widgets/app_icon.dart';
+import 'package:virkey/common_widgets/app_text_form_field.dart';
+import 'package:virkey/features/cloud_synchronisation/authentication.dart';
+import 'package:virkey/features/cloud_synchronisation/cloud_provider.dart';
+import 'package:virkey/utils/overlay.dart';
+import 'package:virkey/common_widgets/app_text.dart';
+import 'package:virkey/constants/colors.dart';
+import 'package:virkey/constants/fonts.dart';
+import 'package:virkey/utils/snackbar.dart';
+
+class UpdateUsernameOverlay {
+  final BuildContext context;
+  final TickerProvider vsync;
+
+  UpdateUsernameOverlay({
+    required this.context,
+    required this.vsync,
+  });
+
+  void close() {
+    _overlay.close();
+  }
+
+  void open() {
+    _overlay.open();
+  }
+
+  final GlobalKey<FormState> _updateEmailFormKey = GlobalKey<FormState>();
+  final FocusNode _updateUsernameFocusNode = FocusNode();
+
+  String _username = '';
+
+  late final AppOverlay _overlay = AppOverlay(
+    context: context,
+    vsync: vsync,
+    children: [
+      Padding(
+        padding: const EdgeInsets.all(11),
+        child: SizedBox(
+          width: MediaQuery.of(context).size.width,
+          height: 40,
+          child: Stack(
+            children: [
+              const Positioned.fill(
+                  left: 0,
+                  right: 0,
+                  child: Center(
+                    child: AppText(
+                      text: 'Username',
+                      size: 30,
+                      family: AppFonts.secondary,
+                    ),
+                  )),
+              Positioned(
+                left: 0,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: AppIcon(
+                      icon: HeroIcons.arrowUturnLeft,
+                      color: AppColors.dark,
+                      onPressed: () => close()),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+      Flexible(
+        fit: FlexFit.loose,
+        child: Padding(
+          padding: const EdgeInsets.all(11),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Padding(
+                padding:
+                    MediaQuery.of(context).orientation == Orientation.landscape
+                        ? EdgeInsets.symmetric(
+                            horizontal: MediaQuery.of(context).size.width * .16)
+                        : EdgeInsets.zero,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Material(
+                      color: Colors.transparent,
+                      child: Form(
+                        key: _updateEmailFormKey,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.stretch,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.all(10.0),
+                              child: Column(
+                                children: [
+                                  AppTextFormField(
+                                    focusNode: _updateUsernameFocusNode,
+                                    labelText: 'New Username',
+                                    onChanged: (value) =>
+                                        {_username = value ?? ''},
+                                    onSaved: (value) =>
+                                        {_username = value ?? ''},
+                                    validator: (value) {
+                                      if (value == null || value.isEmpty) {
+                                        return 'Please enter a username!';
+                                      }
+                                      return null;
+                                    },
+                                    textInputAction: TextInputAction.done,
+                                  ),
+                                ],
+                              ),
+                            ),
+                            const SizedBox(
+                              height: 30,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+      Padding(
+        padding: const EdgeInsets.all(11),
+        child: Row(
+          children: [
+            Expanded(
+              child: AppButton(
+                appText: const AppText(
+                  text: 'Update',
+                  color: AppColors.white,
+                  size: 22,
+                  letterSpacing: 5,
+                ),
+                onPressed: () async {
+                  FocusManager.instance.primaryFocus?.unfocus();
+
+                  if (_updateEmailFormKey.currentState!.validate()) {
+                    _updateEmailFormKey.currentState!.save();
+
+                    List response =
+                        await AppAuthentication.updateUsername(_username);
+
+                    AppSnackBar(
+                            message: response[1],
+                            context: context,
+                            vsync: vsync)
+                        .open();
+
+                    if (response[0]) {
+                      close();
+                    }
+                  }
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    ],
+  );
+}
